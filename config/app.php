469b4b9dbc6338ff4d2efefdc95ceac67fabdff3
John Grubb <grubb@platform.sh>
2016-10-25 17:00:23 -0400

    adding laravel ide helper, doing some config stuff

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
