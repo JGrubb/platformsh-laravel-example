a3c3d77191bc98f7d827be5d783354cbb28894c8
John Grubb <grubb@platform.sh>
2016-11-30 16:04:07 -0500

    sitemap and begin link.  Commit often

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class LinksController extends Controller
 {
-    //
+
+    public function __construct(Request $request)
+    {
+        $this->middleware('auth', [
+            'except' => ['index']
+        ]);
+    }
+
+    public function index() {
+        return view('links/index');
+    }
+
+    public function create() {
+        return 'foo';
+    }
+
+    public function store(Request $request) {
+        dd($request);
+    }
+
+    public function delete($id) {
+        dd($id);
+    }
 }
